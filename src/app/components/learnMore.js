ed6c2ca8e75693b0d54e7fa7cc5f6f1319212702
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-05-09 17:29:18 +0530

    datasphere page developed

--- src/app/components/learnMore.js
+++ src/app/components/learnMore.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export default function LearnMore() {
+export default function LearnMore({ bgcolor, bordercolor }) {
   return (
     <Link
       href="/contact-us"
-      className="inline-block bg-[#134874] xl:text-[20px] text-[16px] hover:bg-[#ffffff] hover:text-[#134874] border-[1px] border-[solid] border-[#134874] font-semibold text-white py-3 px-8 transition duration-300 mt-6"
+      className={`inline-block bg-[${bgcolor ? bgcolor : "#134874"}] xl:text-[20px] text-[16px] hover:bg-[#ffffff] hover:text-[#134874] border-[1px] border-[solid] border-[${bordercolor ? bordercolor : "#134874"}] font-semibold text-white py-3 px-8 transition duration-300 mt-6`}
     >
       Learn More
     </Link>
