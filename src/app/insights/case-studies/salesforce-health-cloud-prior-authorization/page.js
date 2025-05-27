e7786b4819bad6f9a37780aa16af963f54447e89
alkadips <alka.n@rialtes.com>
2025-05-27 13:50:41 +0530

    updated 4 case studies

--- src/app/insights/case-studies/salesforce-health-cloud-prior-authorization/page.js
+++ src/app/insights/case-studies/salesforce-health-cloud-prior-authorization/page.js
@@ -89,13 +89,13 @@ export default function Page() {
 
         <div className="py-10 bg-white">
           <div className="container mx-auto">
-            <h1 className="text-[#000000] py-6 max-w-4xl">Salesforce Health Cloud, Prior Authorization</h1>
+            <h1 className="text-[#000000] py-6 max-w-4xl leading-tight">Accelerating Prior Authorizations Using Salesforce Health Cloud</h1>
           </div>
           <div className="py-4"></div>
           <div className="container mx-auto">
             <div className="flex flex-col md:flex-row justify-between text-black  max-w-4xl xl:w-1/2">
               <div className='pb-6'>
-                <span className='text-[#0092E0]'>Healthcare And Life Sciences</span> <span className='text-[#ACACAC]'> | </span>25 Oct 2024
+                <span className='text-[#0092E0]'>Healthcare And Life Sciences</span> <span className='text-[#ACACAC]'> | </span>27 May 2025
               </div>
               <div className="flex flex-col">
                 <span>8 min read</span>
@@ -116,7 +116,7 @@ export default function Page() {
                   />
                 </a>
               </div>
-              <div className="max-w-[40px]">
+              {/* <div className="max-w-[40px]">
                 <a href="https://www.facebook.com/sharer/sharer.php?u=/case-study-8">
                   <Image
                     src="/images/case-studies/facebook.svg"
@@ -141,7 +141,7 @@ export default function Page() {
                     priority
                   />
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
 
@@ -151,54 +151,57 @@ export default function Page() {
 
 
               <h3 className="h3-bold text-[#0092E0] pb-6">Client Profile</h3>
-              <p className="text-black">A global medical technology company that develops and manufactures innovative products used by doctors to diagnose, treat, and monitor people with cardiovascular and endovascular conditions. This includes pacemakers, defibrillators for regulating heart rhythm and remote monitoring systems for patients with implanted devices. Their main goal is to improve the lives of people diagnosed with heart and blood vessel diseases. </p>
+              <p className="text-black">A global medical technology leader that designs and manufactures life-saving cardiovascular and endovascular products such as pacemakers, defibrillators, and remote patient monitoring systems. The company's mission is to enhance and extend the lives of patients with heart and blood vessel conditions through advanced innovation and patient-centric care.</p>
 
               <div className="py-6"></div>
 
               <h3 className="h3-bold text-[#0092E0] pb-6">Problem or Challenge </h3>
 
-              <p className="text-black pb-6">The prior authorization process for medical treatments was complex and time-consuming for the MedTech company. </p>
+              <p className="text-black pb-6">The prior authorization process, a critical step in patient treatment, was a major bottleneck in the company’s healthcare delivery and revenue cycle operations.</p>
 
-              <p className="text-black pb-6">Patients often have multiple insurance plans with varying coverage details. They are often unaware of coverage limitations until after treatment is denied, leading to surprise bills. </p>
+              <ul className="list-disc marker:text-[#0092E0] text-black pl-4 xl:text-[20px] text-[16px]  font-medium">
+                <li className="pb-4">Patients often had multiple policies with varying coverage rules, leading to confusion and delayed approvals.
+                </li>
+                <li className="pb-4">Patients were frequently unaware of non-covered treatments until they received post-treatment denial notifications, resulting in unexpected costs.</li>
+                <li className="pb-4">Hospitals manually gathered medical data from multiple departments, each needing to match specific formats required by various insurers.
+                </li>
+                <li className="pb-4">Reliance on paper forms or scanned PDFs led to processing delays, impacting treatment timelines and revenue collection.
+                </li>
+              </ul>
 
-              <p className="text-black pb-6">Hospitals struggle with extensive paperwork, including collecting medical data from various departments and submitting it in formats specific to each insurance provider. </p>
-
-              <p className="text-black">Traditional paper-based or scanned submissions lead to delays in insurance approvals, impacting patient care. </p>
 
               <div className="py-6"></div>
 
               <h3 className="h3-bold text-[#0092E0] pb-6">Solutions </h3>
 
 
-              <p className="text-black pb-6">Rialtes developed a Salesforce-based solution to address these challenges and improve the prior authorization process for the MedTech company. </p>
+              <p className="text-black pb-6">To streamline and digitize the prior authorization process, Rialtes developed a Salesforce-based solution using Salesforce Health Cloud that connected hospitals, healthcare providers, patients, and insurers within a unified digital workflow. </p>
+              <ul className="list-disc marker:text-[#0092E0] text-black pl-4 xl:text-[20px] text-[16px]">
+                <li className="pb-4"><strong>Unified Digital Platform: </strong> Consolidated insurance details, ICD codes, and treatment information into a single interface accessible to hospitals and care teams.</li>
+                <li className="pb-4"><strong>Medical Input from Physicians:  </strong> Enabled doctors to directly enter required clinical data, ensuring accuracy and completeness. </li>
+                <li className="pb-4"><strong>Digital Patient Consent:  </strong> Integrated electronic consent collection, ensuring transparency and compliance with patient authorization requirements. </li>
+                <li className="pb-4"><strong>Standardized Submissions: </strong>Automated the transmission of complete medical records using industry-standard formats (HL7 and FHIR), compliant with HIPAA regulations.</li>
+                <li className="pb-4"><strong>Condition-Aware UI: </strong> The user interface dynamically adjusts based on the medical condition, guiding users through all required steps to maximize authorization success.</li>
 
-              <p className="text-black pb-6">Patient insurance information, treatment details, and relevant ICD codes were integrated into a single digital platform.</p>
-
-              <p className="text-black pb-6">We enabled doctors to add necessary medical data directly to the platform. </p>
-
-              <p className="text-black pb-6">Clear and transparent patient consent is obtained electronically within the platform now. </p>
-
-              <p className="text-black pb-6">Complete information from all departments is now submitted electronically to insurance providers in industry-standard formats (HL7 and FHIR) ensuring HIPAA compliance. </p>
-
-              <p className="text-black">The UI adapts to the specific medical condition, which guides users through the required information needed for successful authorization.</p>
-            </div>
-          </div>
+              </ul>
+            </div>    </div>
           <div className="py-6"></div>
           <div className="container mx-auto">
             <div className="max-w-4xl">
               <h3 className="h3-bold text-[#0092E0] pb-6">Benefits</h3>
 
 
-              <p className="text-black pb-6">The centralized platform allows communication and data exchange between all parties that are involved. </p>
-
-              <p className="text-black pb-6">Complete and accurate information leads to fewer denials and resubmissions. </p>
-
-              <p className="text-black pb-6">Digital submissions and standardized formats accelerate insurance approvals. </p>
-
-              <p className="text-black pb-6">Increased transparency and quicker approvals lead to a more positive patient experience. </p>
-
-              <p className="text-black">Faster approvals enable quicker reimbursements to hospitals and the MedTech company.</p>
-
+              <ul className="list-disc marker:text-[#0092E0] text-black pl-4 xl:text-[20px] text-[16px] mt-5 font-medium">
+                <li className="pb-4">Streamlined communication between hospitals, providers, patients, and insurers through a single source of truth.
+                </li>
+                <li className="pb-4">Improved accuracy and completeness of submissions reduced insurance denials by 30%, minimizing delays in patient care and revenue loss.</li>
+                <li className="pb-4">Transitioning from paper to digital submissions cuts prior authorization approval times by up to 50%, improving overall care delivery speed.
+                </li>
+                <li className="pb-4">Greater transparency reduced surprise billing and enabled more confident treatment planning.
+                </li>
+                <li className="pb-4">Hospital administrative workload for prior authorization processing decreased by 35%, freeing up staff to focus on patient care.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="py-6"></div>
