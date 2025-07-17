31642af5f2884f55dda90d581209d7f2735c186d
alkadips <alka.n@rialtes.com>
2025-07-17 18:20:28 +0530

    added advantage page qa changes

--- src/app/services/lob/salesforce-marketing-cloud-implementation-consulting/page.js
+++ src/app/services/lob/salesforce-marketing-cloud-implementation-consulting/page.js
@@ -174,7 +174,7 @@ const SalesforceExplore = () => {
                         <div key={index} className="flex flex-col items-start justify-between h-full">
                             <div>
                                 <h3 className="text-[#005BA1] font-semibold 4xl:text-[36px] text-[20px] mb-5 leading-tight pr-[7rem] xl:pr-0 4xl:pr-0">{service.title}</h3>
-                                <p className="text-black mb-6 4xl:text-[22px] text-[16px] leading-tight pr-[5rem] xl:pr-0 4xl:pr-10">{service.description}</p>
+                                <p className="text-black xl:mb-5 4xl:text-[22px] text-[16px] leading-tight pr-[5rem] xl:pr-0 4xl:pr-10">{service.description}</p>
                             </div>
                             <div>
                                 <LearnMore />
@@ -191,7 +191,7 @@ const Services = () => {
         <div className="text-black 2xl:pr-70 xl:pr-10 md:pr-8">
             <div className="grid 4xl:grid-cols-12">
                 <div className="4xl:col-span-12">
-                    <h2 className="text-black mb-6  xl:text-[40px] 4xl:pr-[2rem] xl:pr-80 4xl:text-[60px]  text-[26px]  leading-tight mt-20 xl:mt-0 4xl:mt-0 pr-[4rem]">Advantage+: A Full-Spectrum Marketing Transformation Approach</h2>
+                    <h2 className="text-black mb-6  xl:text-[40px] 4xl:pr-[2rem] xl:pr-80 4xl:text-[60px]  text-[26px]  leading-tight mt-16 xl:mt-0 4xl:mt-0 pr-[4rem]">Advantage+: A Full-Spectrum Marketing Transformation Approach</h2>
                     <p className="4xl:text-[22px] text-[16px] my-12">Our Advantage+ framework ensures that every phase of your marketing journey is expertly guided:</p>
                 </div>
             </div>
@@ -215,7 +215,7 @@ const deepData1 = [
 ]
 const deepData2 = [
     "Datorama (Marketing Cloud Intelligence) for end-to-end marketing analytics and ROI optimization.",
-    "OCustomer Segmentation and Audience Studio for precision-targeting.",
+    "Customer Segmentation and Audience Studio for precision-targeting.",
     "Marketing Cloud Personalization for real-time engagement.",
     "Marketing Analytics & Einstein AI for predictive insights and customer behavior modeling.",
 ]
@@ -308,7 +308,7 @@ export default function Page() {
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
             />
             {/* hero section */}
-            <section className="relative group overflow-hidden pb-20">
+            <section className="relative group overflow-hidden md:pb-[12rem] xs:pb-20 xl:pb-[12rem] 4xl:pb-[12rem] lg:pb-[12rem] pb-20">
                 <div className="xl:block hidden pt-20">
                     <Image
                         src="/images/services/lob/advantageplus/desktop/banner.webp"
@@ -325,7 +325,7 @@ export default function Page() {
                         alt=""
                         fill
                         priority
-                        className="transform transition-transform duration-500 group-hover:scale-110"
+                        className="h-full"
                     />
                 </div>
 
@@ -339,7 +339,7 @@ export default function Page() {
                                 Advantage+
                             </h3>
 
-                            <h2 className="text-white  leading-tight break-words 4xl:text-[60px] xl:text-[40px] text-[26px]  xl:mt-10 4xl:mt-10 mt-5 pr-32 4xl:pr-0 xl:pr-0 md:pr-[25rem]">
+                            <h2 className="text-white  leading-tight break-words 4xl:text-[60px] xl:text-[40px] text-[26px]  xl:mt-10 4xl:mt-10 mt-5 4xl:pr-0 xl:pr-0 md:pr-[22rem] sm:pr-20 xs:pr-16">
                                 Fueling Growth Through Personalized Marketing
                             </h2>
 
@@ -369,14 +369,14 @@ export default function Page() {
                 </div>
             </section>
             {/* advantage+ section */}
-            <div className="custom-container lg:mt-20 md:mt-6 4xl:pr-[457px]">
+            <div className="custom-container lg:mt-20  4xl:pr-[457px]">
                 <section className=" bg-white">
                     <Services />
                 </section>
             </div>
             {/* deep salesforce section */}
             <section>
-                <div className="relative w-full xl:mt-[-53px] mt-16 4xl:mt-10">
+                <div className="relative w-full xl:mt-[40px] mt-16 4xl:mt-10">
                     <div className="w-full xl:h-[1083px]">
                         <div className="xl:block hidden">
                             <Image
@@ -408,7 +408,7 @@ export default function Page() {
                     </div>
                     <div className="col-span-1 bg-[#006FBE]"></div>
                 </div>
-                <div className="grid xl:grid-cols-12 grid-cols-1 absolute 4xl:mt-[-183px] xl:mt-[-225px] mt-0  custom-container">
+                <div className="grid xl:grid-cols-12 grid-cols-1 absolute 4xl:mt-[-183px] xl:mt-[-225px] 2xl:mt-[-250px] mt-0  custom-container">
                     <div className="xl:col-span-6 col-span-12 bg-[#006FBE] xl:px-14 4xl:px-20 px-8 4xl:pb-14 xl:pb-14 text-white ">
                         <UnorderedList arrName={deepData1} ulClassName="list-disc leading-tight xl:space-y-5 xl:pb-16 4xl:pb-16 space-y-3 font-normal marker:font-bold marker:text-2xl pl-5 4xl:text-[20px] text-[16px] xl:mt-8 mt-5 4xl:pr-16 pr-10 xl:pr-0" liClassName="" />
                         <div className="absolute 4xl:mt-[-25px] xl:mt-[-18px] xl:block hidden">
@@ -425,8 +425,8 @@ export default function Page() {
                 </div>
             </section>
             {/* driving business section*/}
-            <section className="xl:mt-64 4xl:mt-[25rem] custom-container 4xl:pl-0 4xl:pr-[192px] xl:pl-0 max-md:px-0 md:mt-[-17rem]">
-                <div className="grid lg:grid-cols-12 grid-cols-1 mt-[52rem] xl:mt-0 4xl:mt-0">
+            <section className="xl:mt-64 4xl:mt-[25rem] custom-container 4xl:pl-0 4xl:pr-[192px] xl:pl-0 max-md:pl-0 md:mt-[-17rem]">
+                <div className="grid lg:grid-cols-12 grid-cols-1 xss:mt-[55rem] xs:mt-[55rem] sm:mt-[40rem] mt-[60rem] md:mt-[54rem]  lg:mt-[20rem] xl:mt-0 4xl:mt-0">
                     <div className="lg:col-span-7 col-span-12 4xl:pr-[60px] xl:pr-20 lg:pr-10">
                         <Image
                             src="/images/services/lob/advantageplus/desktop/driving.webp"
@@ -450,16 +450,16 @@ export default function Page() {
                             ))}
                         </ul>
 
-                        <div className="">
+                        <div className="mt-10">
                             <LearnMore />
                         </div>
                     </div>
                 </div>
             </section>
             {/* reimagine section */}
-            <div className="relative 4xl:mt-[20rem] 4xl:pl-[142px]  pb-20 custom-container mt-[11rem] max-md:px-0 xl:mt-[18rem]">
-                <div className="bg-[#F0F0F0] relative 4xl:pl-[142px] 4xl:h-[1340px] xl:h-[865px]">
-                    <div className="relative z-10 -mt-32 4xl:mr-[-142px] xl:mr-[-100px] xl:ml-[80px] 4xl:ml-0">
+            <div className="relative 4xl:mt-[20rem] 4xl:pl-[142px]  pb-20 custom-container mt-[16rem] max-md:px-0 xl:mt-[18rem]">
+                <div className="bg-[#F0F0F0] relative 4xl:pl-[142px] 4xl:h-[1340px] xl:h-[865px] 2xl:h-[915px]">
+                    <div className="relative z-10 -mt-32 4xl:mr-[-140px] xl:mr-[-70px] xl:ml-[80px] 4xl:ml-0">
                         <div className="xl:block hidden">
                             <Image
                                 src="/images/services/lob/advantageplus/desktop/reimage.webp"
@@ -478,14 +478,14 @@ export default function Page() {
                                 width={0}
                                 height={0}
                                 sizes="100vw"
-                                className="w-full h-auto object-cover absolute 4xl:mt-[-100px] xl:mt-[-100px] max-md:px-[35px]"
+                                className="w-full h-auto object-cover absolute 4xl:mt-[-100px] xl:mt-[-100px] max-md:px-[35px] mt-[-80px]"
                                 priority
                             />
                         </div>
                     </div>
 
                     <div className="relative z-20 mt-10 grid xl:grid-cols-2 gap-10 px-4 xl:px-0 max-md:px-[35px]">
-                        <div className="4xl:mt-[40rem] mt-[23rem]  md:mt-[43rem] xl:mt-[29rem] xl:ml-[5rem] 4xl:ml-0">
+                        <div className="4xl:mt-[40rem] mt-[15rem] xs:mt-[17rem]  md:mt-[43rem] xl:mt-[29rem] xl:ml-[5rem] 4xl:ml-0 2xl:mt-[32rem]">
                             <h2 className="text-[26px] xl:text-[40px] 4xl:text-[60px] leading-tight pr-10 xl:pr-0 4xl:pr-0">
                                 Reimagine B2B & B2C Marketing with AI and Data Cloud
                             </h2>
@@ -496,8 +496,8 @@ export default function Page() {
                                 <LearnMore />
                             </div>
                         </div>
-                        <div className="4xl:mt-[40rem]  xl:mt-[29rem]">
-                            <ul className="list-disc marker:text-black marker:font-bold 4xl:pl-5 4xl:p-10 4xl:pt-0 4xl:pb-5 text-[16px] 4xl:text-[22px] leading-relaxed space-y-3 pr-12 xl:pr-20 4xl:pr-24">
+                        <div className="4xl:mt-[40rem]  xl:mt-[29rem] 2xl:mt-[32rem]">
+                            <ul className="list-disc marker:text-black marker:font-bold 4xl:pl-5 4xl:p-10 4xl:pt-0 4xl:pb-5 text-[16px] 4xl:text-[22px] space-y-3 pr-12 xl:pr-20 4xl:pr-24">
                                 {aiMarketingData.map((item, index) => (
                                     <li key={index}>
                                         <strong>{item.title}</strong> {item.description}
@@ -538,15 +538,15 @@ export default function Page() {
                         priority
                     />
                 </div>
-                <div className="grid xl:grid-cols-12 grid-cols-12 absolute xl:top-[-8rem] 4xl:top-[-8rem] top-[31rem] md:top-[57rem]">
+                <div className="grid xl:grid-cols-12 grid-cols-12 absolute xl:top-[-8rem] 4xl:top-[-8rem]  md:top-[57rem]">
                     <div className="xl:col-span-6 col-span-12"></div>
                     <div className="xl:col-span-5 col-span-12 bg-[#184671] text-white pt-12 4xl:px-14 xl:px-10 px-10">
                         <h2 className="4xl:text-[50px] text-[26px] xl:text-[38px] leading-tight font-light">Introducing AgentChat</h2>
-                        <h3 className="4xl:text-[38px] text-[24px] xl:text-[25px] mt-5 leading-tight pr-12 xl:pr-0 4xl:pr-0">Transforming WhatsApp Conversations for Salesforce Users</h3>
+                        <h3 className="4xl:text-[35px] text-[24px] xl:text-[25px] mt-5 leading-tight pr-12 xl:pr-0 4xl:pr-0">Transforming WhatsApp Conversations for Salesforce Users</h3>
                         <h3 className="mt-5 leading-tight 4xl:text-[30px] text-[20px] pr-14 xl:pr-0 4xl:pr-0">Revolutionize Customer Engagement with WhatsApp + Salesforce</h3>
                         <p className="mt-5 leading-tight font-light 4xl:text-[22px] text-[16px] pr-12 xl:pr-0 4xl:pr-0">Meet AgentChat, Rialtes’ proprietary bi-directional WhatsApp conversational app built natively on the Salesforce Platform — designed to accelerate customer interactions across any Salesforce Cloud.</p>
                         <h3 className="mt-5 4xl:text-[30px] text-[20px]">Key Features of AgentChat:</h3>
-                        <ul className="list-disc marker:text-white marker:font-bold pl-5 text-[16px] 4xl:text-[20px] leading-relaxed 4xl:space-y-5 xl:space-y-4 space-y-4 mt-5 pr-8 xl:pr-0 4xl:pr-16">
+                        <ul className="list-disc marker:text-white marker:font-bold pl-5 text-[16px] 4xl:text-[20px] leading-tight  4xl:space-y-5 xl:space-y-4 space-y-4 mt-5 pr-8 xl:pr-0 4xl:pr-16">
                             {agentChatData.map((feature, index) => (
                                 <li key={index}>
                                     <strong>{feature.title}</strong> {feature.description}
@@ -561,7 +561,13 @@ export default function Page() {
                 </div>
             </section>
             {/* your growth section */}
-            <section className="4xl:mt-[14rem] custom-container relative 4xl:pr-[142px] 4xl:pl-[213px] xl:mt-80 mt-[78rem] md:mt-[58rem] max-md:px-0">
+            <section className="4xl:mt-[14rem] 2xl:mt-[10rem] custom-container relative 4xl:pr-[142px]
+    mt-[88rem] 
+  xs:mt-[72rem] 
+  sm:mt-[58rem] 
+  md:mt-[52rem] 
+  lg:mt-[80rem] 
+  xl:mt-[15rem]  max-md:px-0">
                 <div className="grid grid-cols-1 xl:grid-cols-2 items-stretch relative">
                     <div className="relative z-0 order-1 xl:order-2 4xl:ml-[-300px] xl:ml-[-211px]">
                         <div className="xl:block hidden">
@@ -588,7 +594,7 @@ export default function Page() {
                         </div>
                     </div>
                     <div className="bg-[#3991AB] text-white z-10 4xl:p-10 4xl:pl-14 relative 4xl:pb-28 xl:pb-20 4xl:pt-10 order-2 xl:order-1 max-md:px-[35px] md:px-[35px] xl:pl-12 xl:pt-8 xl:mb-[70px] 4xl:mb-20">
-                        <h2 className="text-[26px] xl:text-[40px] 4xl:text-[60px] leading-tight pt-10 xl:pt-0 4xl:pt-0">
+                        <h2 className="text-[26px] xl:text-[40px] 4xl:text-[55px] leading-tight pt-10 xl:pt-0 4xl:pt-0">
                             Your Growth Partners
                         </h2>
                         <h3 className="mt-4 xl:mt-3 text-[20px] xl:text-[26px] 4xl:text-[36px] font-semibold pr-20 leading-tight xl:pr-0 4xl:pr-0">
