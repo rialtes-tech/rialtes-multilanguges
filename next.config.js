f17cc99a83f3965a522ec745ae50aee8131bb461
alkadips <alka.n@rialtes.com>
2025-07-22 16:26:24 +0530

    removed unop

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-   images: {
-    unoptimized: true,        
-  },
   webpack: (config) => {
     config.externals = config.externals || {};
     return config;
