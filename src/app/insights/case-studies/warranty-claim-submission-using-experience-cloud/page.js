a3a18d03d8e0257650cd868eff750badca5751f5
Nikita Kothiwala <nikita.k@rialtes.com>
2025-07-31 19:21:29 +0530

    5 case studies text responsive

--- src/app/insights/case-studies/warranty-claim-submission-using-experience-cloud/page.js
+++ src/app/insights/case-studies/warranty-claim-submission-using-experience-cloud/page.js
@@ -104,14 +104,25 @@ export default function Page() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
       />
 
-      <section className="relative h-[250px] sm:h-[500px] lg:h-[650px] overflow-hidden">
-        <Image
+      <section className="relative h-[350px] md:h-[500px]  4xl:h-[650px]  overflow-hidden">
+      <div className="hidden md:block">
+          <Image
           src="/images/case-studies/adaptis_web-banner.webp"
           alt=""
           fill
           style={{ objectFit: "cover", objectPosition: "90% 20%" }}
           priority
         />
+      </div>
+       <div className="block md:hidden">
+          <Image
+          src="/images/case-studies/Adaptis_featured CS.webp"
+          alt=""
+          fill
+          style={{ objectFit: "cover", objectPosition: "90% 20%" }}
+          priority
+        />
+      </div>
       </section>
 
       <section
@@ -119,13 +130,13 @@ export default function Page() {
       custom-container"
       >
 
-        <div className="py-10 bg-white 4xl:max-w-[1200px] 4xl:w-[1200px] xl:w-[1000px]">
+        <div className="py-10 bg-white xl:max-w-[1184px] 4xl:w-[1100px] 2xl:w-[900px] xl:w-[800px] ">
           <div className=" ">
-            <h1 className="text-[#000000] py-6 leading-tight text-[24px] 4xl:text-[60px] xl:text-[45px] ">40% Enhanced Adoption of Warranty Application Using Salesforce Experience Cloud Capability</h1>
+            <h1 className="text-[#000000] py-6 leading-tight text-[26px] 4xl:text-[60px] 2xl:text-[48px]   xl:text-[42px] md:text-[28px]">40% Enhanced Adoption of Warranty Application Using Salesforce Experience Cloud Capability</h1>
           </div>
           <div className="py-4"></div>
           <div className="">
-            <div className="flex flex-col md:flex-row justify-between text-black  xl:max-w-[1084px] xl:w-[1084px]">
+            <div className="flex flex-col md:flex-row justify-between text-black items-center ">
               <div className='pb-6'>
                 <span className='text-[#0092E0]'>Manufacturing</span> <span className='text-[#ACACAC]'> | </span>10 June 2025
               </div>
@@ -134,7 +145,7 @@ export default function Page() {
               </div>
             </div>
             <div className="pt-4"></div>
-             <div className="flex flex-row gap-6 ml-[-8px]">
+             <div className="flex flex-row gap-6 ml-[-8px] justify-center md:justify-start">
                   <div className="max-w-[40px]">
                     <a
                       href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(fullUrl)}&title=A%20public%20housing%20in%20US&summary=Summary%20of%20the%20case%20study&source=LinkedIn`}
@@ -176,26 +187,26 @@ export default function Page() {
             <div className="">
 
 
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px]  pb-6">Client Profile</h2>
-              <p className="text-black">A top manufacturer of roofing products known for innovation and quality. With decades of experience, they specialize in asphalt shingles, roofing accessories, waterproofing solutions, and underlayments for both residential and commercial use. Committed to sustainability, they invest in advanced manufacturing technologies and maintain a broad distribution network, ensuring that builders, contractors, and homeowners have access to premium roofing solutions.</p>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[30px] 2xl:text-[24px] xl:text-[21px] text-[23px] leading-tight  pb-4">Client Profile</h2>
+              <p className="text-black 4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px]">A top manufacturer of roofing products known for innovation and quality. With decades of experience, they specialize in asphalt shingles, roofing accessories, waterproofing solutions, and underlayments for both residential and commercial use. Committed to sustainability, they invest in advanced manufacturing technologies and maintain a broad distribution network, ensuring that builders, contractors, and homeowners have access to premium roofing solutions.</p>
 
               <div className="py-6"></div>
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px]  pb-6">Challenges </h2>
-              <p className="text-black ">The client needed a structured warranty claim submission process, as no system was in place. They required a user-friendly portal integrated with Salesforce to streamline claim submissions while ensuring accurate address validation. Additionally, the absence of automation for notifications and seamless data integration with SAP and other ERP systems led to inefficiencies in claim tracking and management.</p>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[30px] 2xl:text-[24px] xl:text-[21px] text-[23px] leading-tight pb-4">Challenges </h2>
+              <p className="text-black 4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px] ">The client needed a structured warranty claim submission process, as no system was in place. They required a user-friendly portal integrated with Salesforce to streamline claim submissions while ensuring accurate address validation. Additionally, the absence of automation for notifications and seamless data integration with SAP and other ERP systems led to inefficiencies in claim tracking and management.</p>
 
               <div className="py-6"></div>
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px]  pb-6">Solutions </h2>
-              <p className="text-black pb-6">Rialtes implemented a Salesforce Experience Builder portal with an LWC-based UI to streamline the warranty claim submission process. To ensure accurate address capture, we integrated Google API Address Validation. </p>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[30px] 2xl:text-[24px] xl:text-[21px] text-[23px] leading-tight pb-4">Solutions </h2>
+              <p className="text-black pb-4 4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px]">Rialtes implemented a Salesforce Experience Builder portal with an LWC-based UI to streamline the warranty claim submission process. To ensure accurate address capture, we integrated Google API Address Validation. </p>
 
-              <p className="text-black pb-6">Carried out automated workflows that eliminated manual intervention, enabling seamless claim handling. Real-time email and SMS notifications kept customers informed about their claim status, enhancing transparency. </p>
+              <p className="text-black pb-4 4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px]">Carried out automated workflows that eliminated manual intervention, enabling seamless claim handling. Real-time email and SMS notifications kept customers informed about their claim status, enhancing transparency. </p>
 
-              <p className="text-black">Additionally, we established SAP and ERP integrations for smooth data exchange, reducing manual effort and significantly improving claim processing efficiency.</p>
+              <p className="text-black 4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px]">Additionally, we established SAP and ERP integrations for smooth data exchange, reducing manual effort and significantly improving claim processing efficiency.</p>
 
               <div className="py-6"></div>
 
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px]  pb-6">Benefits</h2>
-             <div className="pl-2">
-               <ul className="list-disc marker:text-[#0092E0] text-black pl-4 xl:text-[20px] text-[16px] font-medium">
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[30px] 2xl:text-[24px] xl:text-[21px] text-[23px] leading-tight pb-4">Benefits</h2>
+             <div className="pl-4">
+               <ul className="list-disc marker:text-[#0092E0] text-black  4xl:text-[20px]  2xl:text-[18px]  xl:text-[17px] text-[16px] font-medium">
                 <li className="pb-4">Reduced claim submission time by 60%, allowing customers to complete requests in under 5 minutes through the intuitive web portal.</li>
                 <li className="pb-4">Address validation via Google API reduced incorrect or incomplete address entries by over 85%, eliminating delays caused by manual corrections.</li>
                 <li className="pb-4">Automated workflows decreased manual processing, freeing up internal teams to focus on more strategic tasks.</li>
@@ -212,7 +223,7 @@ export default function Page() {
 
       {/* Latest Blogs */}
       <div
-        className="py-10
+        className="py-10 lg:pr-0
         custom-container"
       >
         <RelatedTopicsCarousel slides={slides} />
