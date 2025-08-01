71f63649c4d109496ce1c0c65442582aaf9c91c4
alkadips <alka.n@rialtes.com>
2025-08-01 14:04:40 +0530

    added 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,18 +1,48 @@
-// pages/404.tsx
 "use client";
 
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Custom404() {
+  const router = useRouter()
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-center px-4">
-      <h1 className="text-6xl font-bold text-blue-600">404</h1>
-      <p className="text-xl mt-4 text-gray-700">
-        Oops! The page you are looking for doesn't exist.
-      </p>
-      <Link href="/" className="mt-6 inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">
-        Go Back Home
-      </Link>
+    <div className="grid xl:grid-cols-12  px-4 pt-10 custom-container xl:gap-20">
+      <div className="xl:col-span-7 col-span-12">
+        <div className="xl:block hidden">
+          <Image
+            src="/images/not-found/desk.webp"
+            alt="404 Error"
+            className="w-full h-auto object-contain"
+            priority
+            width={0}
+            height={0}
+          />
+        </div>
+        <div className="xl:hidden block">
+          <Image
+            src="/images/not-found/404-mobile.webp"
+            alt="404 Error"
+            className="w-full h-auto object-contain"
+            priority
+            width={0}
+            height={0}
+          />
+        </div>
+      </div>
+
+      <div className="xl:col-span-5 col-span-12 text-center xl:text-left  !not-found-mt mb-10 xl:mb-0">
+        <h1 className="4xl:text-[81px] xl:text-[61px] text-[41px]">OOPS!</h1>
+        <h2 className="4xl:text-[48px] xl:text-[38px] text-[#0A6BB8] mb-4 font-medium text-[18px]">PAGE NOT FOUND</h2>
+        <p className="4xl:text-[22px] xl:text-[20px] text-[16px] mb-6">
+          Sorry, the page you’re looking for doesn’t exist. If you think something is broken, report a problem.
+        </p>
+       
+          <button  onClick={() => router.push('/')} className="bg-[#134874] text-white text-sm md:text-base font-semibold mt-5 py-3 px-6 md:px-8 border border-[#134874] hover:bg-white hover:text-[#134874] transition duration-300">
+            Return To Home
+          </button>
+       
+      </div>
     </div>
-  )
+  );
 }
