5b2160684c125b5ff5cbe96e744b530ed415d90a
alkadips <alka.n@rialtes.com>
2025-08-25 17:57:30 +0530

    resolve conflict

--- src/app/page.js
+++ src/app/page.js
@@ -268,98 +268,6 @@ const Home = () => {
                         </div>
                     </div> */}
                 </div>
-
-                {/* //innovating section */}
-                <section className='custom-container xl:mt-[98px] mt-[55px]'>
-                    <Image
-                        priority
-                        height='100'
-                        width='280'
-                        alt='Success Stories'
-                        className='object-contain w-full h-full transform group-hover:scale-110 group-hover:origin-center transition-transform duration-500'
-                        src='/images/homepage/innovative2.webp'
-                        fetchPriority='high'
-                    />
-                    <div
-                        ref={refs[7]}
-                        className={`transition-all duration-1000 ease-out transform grid xl:grid-cols-12 grid-cols-1 ${inViews[7] ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-                            }`}
-
-                    >
-                        <div className='lg:col-span-4 col-span-12 lg:border-r-[2px] border-black-900'></div>
-
-                        <div className='lg:col-span-8 col-span-12 2xl:pl-[78px] lg:pl-[42px]'>
-                            <h3 className="4xl:text-[110px] xl:text-[80px] md:text-[60px] sm:text-[46px] font-bold text-[33px] outline-text border-l-[2px] xl:border-none border-gay-300 ml-[34%] sm:ml-[35%] md:ml-[32%] pl-[20px] xl:pl-0 xl:ml-auto mt-[20px] leading-tight">with Impact</h3>
-                            <h2 className='4xl:text-[32px] xl:text-[28px] text-[26px] text-[#0077CE] xl:mt-[30px] mt-[30px] font-bold leading-tight'>Beyond Business: A Commitment to Change</h2>
-                            <p className='xl:mt-[27px] mt-[16px] 4xl:text-[20px] xl:text-[18px] text-[16px] leading-tight'>Rialtes is more than a technology leader — we are advocates for positive change. From sustainability initiatives to empowering underrepresented voices, we’re dedicated to making a meaningful impact</p>
-                        </div>
-
-                    </div>
-
-                </section>
-
-                {/* success stories section */}
-                <section className='custom-container lg:pr-0 xl:mt-[147px] mt-[87px]'>
-                    <div
-
-                        ref={refs[0]}
-                        className={`col-span-4 transition-all duration-1000 ease-out transform items-center grid xl:grid-cols-12 grid-cols-1 ${inViews[0] ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-                            }`}
-
-                    >
-                        <div className='col-span-5'>
-                            <h2 className='4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight'>Success Stories</h2>
-                        </div>
-                        <div className='col-span-7'>
-                            <p className='mt-5 xl:mt-0 md:mt-5 lg:mt-mt-5 pr-8 xl:pr-0 lg:w-[80%] 4xl:text-[20px] xl:text-[18px] text-[16px]'>Discover how we’ve harnessed the disruptive power of cutting-edge AI to help companies anticipate and act with insight and speed with IT consulting services.</p>
-
-                        </div>
-                        <div className='col-span-3'></div>
-
-                    </div>
-                    <div className='grid lg:grid-cols-4 sm:grid-cols-2 mt-16 sm:gap-[20px] gap-y-[22px]'>
-                        {successStoryData.map((success, index) => {
-                            // const isLight = success.theme === "light";
-                            return (
-                                <React.Fragment key={index}>
-                                    <Link href={success.url}>
-                                        <div className="relative  4xl:h-[486px] lg:h-[360px] h-[391px] max-[375px]:h-[350px] max-[320px]:h-[300px] overflow-hidden group shadow-lg">
-                                            <div
-                                                className="absolute inset-0 transform scale-[1] w-full origin-bottom-left transition-transform duration-300 ease-in-out group-hover:scale-[1.9]"
-                                            >
-                                                <Image
-                                                    className="w-full h-auto"
-                                                    src={success.imageUrl}
-                                                    alt="life at rialtes"
-                                                    width={0}
-                                                    height={0}
-                                                    sizes="100vw"
-                                                    priority
-                                                    quality={60}
-                                                    fetchPriority='high'
-                                                />
-
-                                            </div>
-
-                                            <div className="absolute inset-0 hover:text-white bg-black bg-opacity-10 group-hover:bg-opacity-50 transition duration-700"></div>
-                                            <div className={`relative z-10 p-6 text-white`}>
-                                                <p className="mb-5 text-[18px] ">{success.title}</p>
-                                                <div className="font-medium leading-tight text-[20px] 4xl:text-[26px] xl:text-[22px] pr-10 xl:pr-5">
-                                                    {success.description}
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        {(index + 1) % 4 === 0 && (
-                                            <div className="w-full xl:col-span-4"></div>
-                                        )}
-                                    </Link>
-                                </React.Fragment>
-                            );
-                        })}
-                    </div>
-
-                </section>
             </div>
 
             {/* innovating section */}
@@ -370,7 +278,7 @@ const Home = () => {
                     width='280'
                     alt='Success Stories'
                     className='object-contain w-full h-full transform group-hover:scale-110 group-hover:origin-center transition-transform duration-500'
-                    src='/images/homepage/AdobeStock_406847557.svg'
+                    src='/images/homepage/innovative2.webp'
                     fetchPriority='high'
                 />
                 <div
