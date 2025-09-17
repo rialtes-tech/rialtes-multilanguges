152ae300eea183feb15136b82b38e6abe1bcab35
alkadips <alka.n@rialtes.com>
2025-09-17 11:41:18 +0530

    updated request

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,40 +1,39 @@
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
-import fs from "fs";
-import path from "path";
-
+ 
 async function importMessages(locale, files) {
-  const messages = {};
-  for (const file of files) {
-    const filePath = path.join(process.cwd(), "messages", locale, `${file}.json`);
-    const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-    Object.assign(messages, data);
-  }
-  return messages;
+    const messages = {};
+    for (const file of files) {
+        const data = (await import(`../../messages/${locale}/${file}.json`)).default;
+        Object.assign(messages, data);
+    }
+    return messages;
 }
-
+ 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
-    : routing.defaultLocale;
-
-  const allMessages = await Promise.all([
-    importMessages(locale, ["homepage"]),
-    importMessages(locale, ["products"]),
-    importMessages(locale, ["solutions"]),
-    importMessages(locale, ["aboutus"]),
-    importMessages(locale, ["services"]),
-    importMessages(locale, ["industry"]),
-    importMessages(locale, ["insight"]),
-    importMessages(locale, ["webinars"]),
-    importMessages(locale, ["blogs"]),
-    importMessages(locale, ["caseStudy"]),
-  ]);
-
-  return {
-    locale,
-    messages: Object.assign({}, ...allMessages),
-  };
+    const requested = await requestLocale;
+    const locale = hasLocale(routing.locales, requested)
+        ? requested
+        : routing.defaultLocale;
+ 
+    const allMessages = await Promise.all([
+        importMessages(locale, ["homepage"]),
+        importMessages(locale, ["products"]),
+        importMessages(locale, ["solutions"]),
+        importMessages(locale, ["aboutus"]),
+        importMessages(locale, ["services"]),
+        importMessages(locale, ["industry"]),
+        importMessages(locale, ["insight"]),
+        importMessages(locale, ["webinars"]),
+        importMessages(locale, ["blogs"]),
+        importMessages(locale, ["caseStudy"]),
+    ]);
+ 
+    return {
+        locale,
+        messages: Object.assign({}, ...allMessages),
+    };
 });
+ 
+ 
