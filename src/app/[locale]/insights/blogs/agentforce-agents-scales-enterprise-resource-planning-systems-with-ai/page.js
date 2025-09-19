fde86b41c920c8475bf10c424263cff506d13d55
pallavi gawande <pallavi.g@rialtes.com>
2025-09-19 19:09:09 +0530

    4 pages done

--- src/app/[locale]/insights/blogs/agentforce-agents-scales-enterprise-resource-planning-systems-with-ai/page.js
+++ src/app/[locale]/insights/blogs/agentforce-agents-scales-enterprise-resource-planning-systems-with-ai/page.js
@@ -59,21 +59,34 @@ export default function Page() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
       />
 
-      <section className="relative h-[250px] sm:h-[500px] lg:h-[650px] overflow-hidden">
-        <Image
+      <section className="relative  h-[350px] md:h-[400px]  4xl:h-[650px] 2xl:h-[500px] ">
+        <div className="hidden md:block">
+          <Image
           src="/images/blog/agentforce-erp-scaling-ai.webp "
           alt="Futuristic robotic hand on a laptop representing AI’s role in revolutionizing ERP systems through Agentforce innovation."
           fill
           style={{ objectFit: "cover", objectPosition: "50% 20%" }}
           priority
         />
+
+      </div>
+       <div className="md:hidden block">
+          <Image
+          src="/images/blog/blog-12.webp"
+          alt="Futuristic robotic hand on a laptop representing AI’s role in revolutionizing ERP systems through Agentforce innovation."
+          fill
+          style={{ objectFit: "cover", objectPosition: "50% 20%" }}
+          priority
+        />
+        
+      </div>
       </section>
 
       <section className="custom-container">
 
-        <div className="py-10 bg-white">
-          <div className="container mx-auto">
-            <div className="flex flex-col md:flex-row justify-between text-black items-center   max-[1084px] xl:w-[1084px]">
+        <div className="py-10 bg-white 4xl:w-[1084px] 2xl:w-[900px] xl:w-[800px]">
+          <div className="">
+            <div className="flex flex-col md:flex-row justify-between text-black items-center">
               <div className='sm:mb-0 mb-6'>
                 <span className='text-[#0092E0]'>{t('blogTopic')}</span> <span className='text-[#ACACAC]'> | </span>13 Jan 2025
               </div>
@@ -116,64 +129,64 @@ export default function Page() {
             </div>
           </div>
           <div className="pt-6"></div>
-          <div className="container pl-0 mx-auto">
-            <h1 className="text-[#000000] font-semibold pb-6 4xl:w-[1084px] xl:w-[884px] text-[26px] xl:text-[40px] 4xl:text-[60px] leading-tight">{t('blogTitle')}</h1>
+          <div className="pl-0">
+            <h1 className="text-[#000000] pb-6 leading-tight text-[26px] 4xl:text-[60px]  2xl:text-[48px]   xl:text-[42px]  md:text-[28px]">{t('blogTitle')}</h1>
           </div>
 
-          <div className="container pl-0 mx-auto">
-            <div className=" max-[1084px] xl:w-[1084px]">
+          <div className="pl-0">
+            <div className="">
 
-              <p className="text-black">{t('blogMainData')}</p>
-              <p className="text-black pt-4">{t('blogMainData2')}</p>
+              <p className="text-black 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('blogMainData')}</p>
+              <p className="text-black pt-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('blogMainData2')}</p>
               <div className="py-4"></div>
               {/* agentforce integrated */}
 
-              <h2 className="font-medium text-[#0092E0] xl:text-[35px] text-[25px] pb-4"> {t('agentForceTitle')}</h2>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[32px] xl:text-[26px] text-[22px] leading-tight pb-4"> {t('agentForceTitle')}</h2>
 
-              <p className="text-black">{t('agentForceDesc')}
+              <p className="text-black 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('agentForceDesc')}
                 <Link className="text-[#0092E0] underline transition duration-300 ease-out hover:text-gray-400" href="https://www.rialtes.com/solutions/data-ai/salesforce-data-cloud-consulting/">
                   {t('agentForceLink')}</Link>, {t('agentForceDesc2')} </p>
 
               <div className="py-4"></div>
 
               {/*  how does agentforce section */}
-              <h3 className="h3-bold  pb-4"> {t('howDoesTitle')}</h3>
+              <h3 className="h3-bold  pb-4 4xl:text-[24px]  2xl:text-[21px]  xl:text-[20px] text-[20px] leading-tight"> {t('howDoesTitle')}</h3>
 
-              <p className="text-black pb-4"> {t('howDoesDesc')}</p>
+              <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight"> {t('howDoesDesc')}</p>
 
-              <p className="text-black"> {t('howDoesDesc2')}</p>
+              <p className="text-black 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight"> {t('howDoesDesc2')}</p>
 
               <div className="py-4"></div>
 
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px] pb-4">{t('benefitsTitle')}</h2>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[32px] xl:text-[26px] text-[22px] leading-tight pb-4">{t('benefitsTitle')}</h2>
               {
                 benefitsData.map((data, ind) => {
                   return (
                     <div key={ind}>
-                      <h3 className="h3-bold pb-4">{data.title}</h3>
-                      <p className="text-black pb-4">{data.desc}</p>
+                      <h3 className="h3-bold pb-4 4xl:text-[24px]  2xl:text-[21px]  xl:text-[20px] text-[20px] leading-tight">{data.title}</h3>
+                      <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{data.desc}</p>
                     </div>
                   )
                 })
               }
 
-              <h3 className="h3-bold pb-4">{t('enhancedTitle')}</h3>
+              <h3 className="h3-bold pb-4 4xl:text-[24px]  2xl:text-[21px]  xl:text-[20px] text-[20px] leading-tight">{t('enhancedTitle')}</h3>
 
-              <p className="text-black">{t('enhancedDesc')}
-                <Link className="text-[#0092E0] underline transition duration-300 ease-out hover:text-gray-400" href="https://www.rialtes.com/insights/blogs/agents-as-teammates-revolutionizing-slack-collaboration-with-agentforce/?utm_source=internal&utm_medium=blog">{t('enhancedLink')} </Link>{t('enhancedDesc2')} </p>
+              <p className="text-black 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('enhancedDesc')}
+                <Link className="text-[#0092E0] underline transition duration-300 ease-out hover:text-gray-400" href="https://www.rialtes.com/insights/blogs/agents-as-teammates-revolutionizing-slack-collaboration-with-agentforce/?utm_source=internal&utm_medium=blog">{t('enhancedLink')}</Link> {t('enhancedDesc2')} </p>
 
 
               <div className="py-4"></div>
 
               {/* application section */}
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px] pb-4">{t('applicationTitle')}</h2>
-              <p className="text-black pb-4">{t('applicationDesc')}</p>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[32px] xl:text-[26px] text-[22px] leading-tight pb-4">{t('applicationTitle')}</h2>
+              <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('applicationDesc')}</p>
               {
                 applicationData.map((data, ind) => {
                   return (
                     <div key={ind}>
-                      <h3 className="h3-bold pb-4">{data.title}</h3>
-                      <p className="text-black pb-4">{data.desc}</p>
+                      <h3 className="h3-bold pb-4 4xl:text-[24px]  2xl:text-[21px]  xl:text-[20px] text-[20px] leading-tight">{data.title}</h3>
+                      <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{data.desc}</p>
                     </div>
                   )
                 })
@@ -181,15 +194,15 @@ export default function Page() {
 
               <div className="py-4"></div>
 
-              <h2 className="font-medium text-[#0092E0] xl:text-[30px] text-[20px] pb-4">{t('futureTitle')}</h2>
-              <p className="text-black pb-4">{t('futureDesc')}  <Link className="text-[#0092E0] underline transition duration-300 ease-out hover:text-gray-400" href="https://www.youtube.com/watch?v=EcPEwPAG2SM&utm_source=internal&utm_medium=blog">{t('futureLink')}</Link> {t('futureDesc2')}</p>
-              <p className="text-black pb-4">{t('futureDesc3')} <Link className="underline" href={"mailto:sales@rialtes.com"}>sales@rialtes.com</Link>.</p>
+              <h2 className="font-medium text-[#0092E0] 4xl:text-[32px] xl:text-[26px] text-[22px] leading-tight pb-4">{t('futureTitle')}</h2>
+              <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('futureDesc')}  <Link className="text-[#0092E0] underline transition duration-300 ease-out hover:text-gray-400" href="https://www.youtube.com/watch?v=EcPEwPAG2SM&utm_source=internal&utm_medium=blog">{t('futureLink')}</Link> {t('futureDesc2')}</p>
+              <p className="text-black pb-4 4xl:text-[20px] xl:text-[17px] text-[16px] leading-tight">{t('futureDesc3')} <Link className="underline" href={"mailto:sales@rialtes.com"}>sales@rialtes.com</Link>.</p>
             </div>
           </div>
         </div>
       </section>
       {/* Latest Blogs */}
-      <div className="custom-container pb-10">
+      <div className="custom-container pb-10 lg:pr-0">
         <BlogsCarousel slides={blogs} />
       </div>
     </div>
