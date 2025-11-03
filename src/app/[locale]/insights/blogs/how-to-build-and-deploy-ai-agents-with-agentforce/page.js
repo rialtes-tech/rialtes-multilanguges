26036eadd0c357c41fb90bc890a18d32a0ff9717
Nikita Kothiwala <nikita.k@rialtes.com>
2025-11-03 11:49:50 +0530

    Insights page & 8 Blogs Image Alt done

--- src/app/[locale]/insights/blogs/how-to-build-and-deploy-ai-agents-with-agentforce/page.js
+++ src/app/[locale]/insights/blogs/how-to-build-and-deploy-ai-agents-with-agentforce/page.js
@@ -143,16 +143,16 @@ export default function Page() {
       <section className="relative 4xl:h-[638px] xl:h-[450px] 2xl:h-[500px] sm:h-[600px] md:h-[700px] overflow-hidden">
         <div className="xl:block hidden">
           <Image
-            src="/images/blog/first-ai-agent-desktop-banner.webp"
-            alt="ai that listens"
+            src="/images/blog/artificial-intelligence-data-visualization.webp"
+            alt={t("bannerAlt")}
             fill
             priority
           />
         </div>
         <div className="xl:hidden block">
           <Image
-            src="/images/blog/first-ai-agent-mob-banner.webp"
-            alt="ai that listens"
+            src="/images/blog/artificial-intelligence-data-visualization-mobile.webp"
+            alt={t("bannerAlt")}
             priority
             height={0}
             width={0}
