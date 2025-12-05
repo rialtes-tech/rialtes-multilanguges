770798be92d7bb330fbe477c289d01d851e5bcb2
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-12-05 16:25:38 +0530

    blog changes

---
+++ src/app/[locale]/insights/blogs/why-patient-drop-out-clinical-trials-and-how-to-prevent-it/page.js
@@ -0,0 +1,224 @@
+"use client";
+import Image from "next/image";
+import FilteredBlogCarousel from '@/app/[locale]/components/FilteredLatestBlogCarousel'
+import useUrl from "@/app/[locale]/components/useUrl";
+import Seo from "@/app/[locale]/components/Seo";
+import Script from "next/script";
+import FAQAccordion from "@/app/[locale]/components/faqAccordion";
+import BlogSocialIcons from '@/app/[locale]/components/blogSocialIcons'
+import UnorderedList from "@/app/[locale]/components/unorderedList";
+import React from "react";
+import { useLocale, useTranslations } from "next-intl";
+import enContent from "../../../../../../messages/en/blogs.json";
+import esContent from "../../../../../../messages/es/blogs.json";
+import frContent from "../../../../../../messages/fr/blogs.json";
+import { changeLocalization } from "@/app/[locale]/components/changeLocalization";
+
+export default function Page() {
+    const fullUrl = "https://www.rialtes.com/insights/blogs/why-patient-drop-out-clinical-trials-and-how-to-prevent-it";
+    const currUrl = useUrl()
+    const t = useTranslations("whyPatient");
+    const locale = useLocale();
+    const blogsContent = changeLocalization(locale, {
+        en: enContent,
+        es: esContent,
+        fr: frContent,
+    });
+    const { mainData, realData, digitalData, retentionData, howMediainaData, faqData, schemaData } = blogsContent.whyPatient
+
+    return (
+        <section className="min-h-screen">
+            <Seo
+                title={t('seoTitle')}
+                description={t('seoDescription')}
+                keywords="Partners, website, welcome"
+                canonical={
+                    `https://www.rialtes.com/${locale}/insights/blogs/why-patient-drop-out-clinical-trials-and-how-to-prevent-it/`
+                }
+            />
+
+            <Script
+                id="schema-provenWays"
+                type="application/ld+json"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+            />
+
+            <section className="relative 4xl:h-[638px] xl:h-[450px] 2xl:h-[500px] sm:h-[600px] md:h-[700px] overflow-hidden">
+                <div className="xl:block hidden">
+                    <Image
+                        src="/images/blog/patient-consultation-genomics.webp"
+                        alt={t('bannerAlt')}
+                        fill
+                        priority
+                    />
+                </div>
+                <div className="xl:hidden block">
+                    <Image
+                        src="/images/blog/patient-consultation-genomics-mobile.webp"
+                        alt={t('bannerAlt')}
+                        priority
+                        height={0}
+                        width={0}
+                        className="w-full h-auto object-cover"
+                    />
+                </div>
+            </section>
+
+            <section className="custom-container 4xl:mt-[80px] xl:mt-[60px] mt-[40px]">
+                <div className="grid lg:grid-cols-12">
+                    <div className="4xl:col-span-10 xl:col-span-10 lg:col-span-11">
+
+                        {/* date and icons */}
+                        <BlogSocialIcons fullUrl={fullUrl} topic="MediAina" date="03 December 2025" />
+
+                        {/* main blog */}
+                        <div className="xl:mt-[60px] mt-[42px]">
+                            <h1 className="4xl:text-[60px] 2xl:text-[48px] xl:text-[42px] md:text-[28px] text-[26px] leading-tight">{t('blogTitle')}</h1>
+
+                            <div className="xl:mt-[38px] mt-[33px]">
+                                {
+                                    mainData.map((data, ind) => {
+                                        return (
+                                            <p
+                                                key={ind}
+                                                className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data }}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
+
+
+                            {/* real reasons section */}
+                            {
+                                realData.map((data, ind) => (
+                                    <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                        <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                        {data.desc && (
+                                            <p
+                                                className="mb-10 mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                        )}
+                                        {
+                                            data.data.map((elem, id) => (
+                                                <React.Fragment key={id}>
+                                                    <h3 className="mt-10 4xl:text-[22px] 2xl:text-[20px] xl:text-[18px] md:text-[16px] text-[16px] font-bold 4xl:w-[90%]">
+                                                        {elem.title}
+                                                    </h3>
+                                                    <p
+                                                        className="mt-3 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                        dangerouslySetInnerHTML={{ __html: elem.desc }}
+                                                    />
+                                                </React.Fragment>
+                                            ))
+                                        }
+
+                                    </div>
+                                ))
+                            }
+
+                            {/* digital section */}
+                            {
+                                digitalData.map((data, ind) => (
+                                    <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                        <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                        {data.desc && (
+                                            <p
+                                                className="mb-10 mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                        )}
+
+                                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:mt-[40px] mt-[40px] gap-[20px] 4xl:w-[1220px] sm:w-[80%] md:w-full">
+                                            {
+                                                data.list.map((elem, id) => {
+                                                    return (
+                                                        <div className="4xl:p-[46px] p-[32px] border border-[#707070]" key={id}>
+                                                            <h3 className="text-[#006FBE] 4xl:text-[24px] 2xl:text-[21px] xl:text-[20px] md:text-[20px] text-[20px] font-bold pb-4 border-b border-[#707070]">{elem.title}</h3>
+                                                            <p className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]" dangerouslySetInnerHTML={{ __html: elem.desc }} />
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+
+                                        <p
+                                            className="mt-10 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                            dangerouslySetInnerHTML={{ __html: data.desc2 }}
+                                        />
+                                    </div>
+                                ))
+                            }
+
+                            {/* retention section */}
+                            {
+                                retentionData.map((data, ind) => (
+                                    <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                        <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                        {data.desc && (
+                                            <p
+                                                className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                        )}
+                                        <UnorderedList arrName={data.list} ulClassName="pl-[30px] lg:pl-[40px] space-y-1 mt-5 space-y-3 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] list-disc font-medium" />
+                                        <p
+                                            className="mt-10 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                            dangerouslySetInnerHTML={{ __html: data.desc2 }}
+                                        />
+                                    </div>
+                                ))
+                            }
+
+                            {/* how meidaina section */}
+                            {
+                                howMediainaData.map((data, ind) => (
+                                    <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                        <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                        <UnorderedList arrName={data.desc} ulClassName="space-y-1 mt-5 space-y-3 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium" />
+                                        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:mt-[80px] mt-[40px] gap-[40px] 4xl:w-[1200px] md:w-full">
+                                            {
+                                                data.list.map((elem, id) => (
+                                                    <div key={id}>
+                                                        <Image
+                                                            src={elem.img}
+                                                            alt={elem.alt}
+                                                            width={0}
+                                                            height={0}
+                                                            className="w-[100px] h-[100px]"
+                                                            priority
+                                                        />
+                                                        <p
+                                                            className="mt-10 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-bold"
+                                                            dangerouslySetInnerHTML={{ __html: elem.desc }}
+                                                        />
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                        <UnorderedList arrName={data.desc2} ulClassName="space-y-1 lg:mt-[80px] mt-[40px] space-y-3 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium" />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* faq section */}
+            <section className="xl:mt-[80px] mt-[40px] custom-container">
+                <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{t('faqTitle')}</h2>
+                <div className="mt-[29px] xl:mt-[30px]">
+                    <FAQAccordion faqData={faqData} />
+                </div>
+            </section>
+
+            {/* blog carousel */}
+            <section className="custom-container lg:pr-0 xl:my-[80px] md:my-[60px] my-[40px]">
+                <FilteredBlogCarousel url={currUrl} />
+            </section>
+        </section>
+    )
+}
