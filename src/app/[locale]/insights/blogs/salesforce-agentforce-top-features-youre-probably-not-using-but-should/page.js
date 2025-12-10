3b0397d0bc78ad07e7365a9e11b4552fa98d7570
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-12-10 18:18:41 +0530

    blogs QA

--- src/app/[locale]/insights/blogs/salesforce-agentforce-top-features-youre-probably-not-using-but-should/page.js
+++ src/app/[locale]/insights/blogs/salesforce-agentforce-top-features-youre-probably-not-using-but-should/page.js
@@ -66,7 +66,7 @@ export default function Page() {
           <div className="4xl:col-span-10 xl:col-span-10 lg:col-span-11">
 
             {/* date and icons */}
-            <BlogSocialIcons fullUrl={fullUrl} topic={t('blogTopic')} date="03 Feb 2025" />
+            <BlogSocialIcons fullUrl={fullUrl} topic={t('blogTopic')} date="03 February 2025" />
 
             {/* main blog */}
             <div className="xl:mt-[60px] mt-[42px]">
