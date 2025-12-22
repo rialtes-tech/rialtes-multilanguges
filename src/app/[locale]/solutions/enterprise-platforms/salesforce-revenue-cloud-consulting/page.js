ba4f56e669395c146f7c0cbe862b8df8d299871c
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-12-22 18:46:28 +0530

    pages QA

--- src/app/[locale]/solutions/enterprise-platforms/salesforce-revenue-cloud-consulting/page.js
+++ src/app/[locale]/solutions/enterprise-platforms/salesforce-revenue-cloud-consulting/page.js
@@ -16,7 +16,7 @@ export default function HealthCloud() {
   const t = useTranslations("revenueCloud");
   const locale = useLocale();
   const content = changeLocalization(locale, { en: enContent, es: esContent, fr: frContent });
-  const { revenueStats, salesForce, salesForce2,schemaData } = content.revenueCloud;
+  const { revenueStats, salesForce, salesForce2, schemaData } = content.revenueCloud;
 
   return (
     <section>
@@ -60,7 +60,7 @@ export default function HealthCloud() {
               <h3 className="text-white text-[14px] md:text-[20px] lg:text-[18px] xl:text-[20px] 2xl:text-[22px] 4xl:text-[24px] font-bold">
                 {t("revenueTitle")}
               </h3>
-              <h2 className="text-white   text-[24px]  md:text-[40px] md:leading-[50px] lg:text-[50px] lg:leading-[73px] mt-[11.5px] md:mt-[10px] xl:text-[55px] 2xl:text-[60px] 4xl:text-[60px] w-[300px] md:w-[490px] lg:w-[600px] xl:w-[700px] 2xl:w-[700px] 4xl:w-[778px] leading-tight ">
+              <h2 className="text-white text-[24px] md:text-[40px] md:leading-[50px] lg:text-[50px] lg:leading-[73px] mt-[11.5px] md:mt-[10px] xl:text-[55px] 2xl:text-[60px] 4xl:text-[60px] w-[300px] md:w-[490px] lg:w-[600px] xl:w-[700px] 2xl:w-[700px] 4xl:w-[778px] leading-tight ">
                 {t("revenueSubTitle")}
               </h2>
               <Image
@@ -69,24 +69,25 @@ export default function HealthCloud() {
                 width={0}
                 height={0}
                 priority
-                className="mt-5 w-[120px] md:w-[180px] lg:w-[190px] xl:w-[200px] 2xl:w-[270px] 4xl:w-[300px]"
+                className="mt-10 w-[100px] md:w-[140px] lg:w-[160px] xl:w-[180px] 2xl:w-[230px] 4xl:w-[260px]"
               />
             </div>
             <div className="col-span-12 lg:col-span-3 xl:col-span-5"></div>
           </div>
         </div>
       </section>
+      {/* page description */}
       <section className="custom-container">
         <div className="py-8 bg-white">
           <div className="2xl:pr-72 xl:pr-40 md:pr-20 4xl:pr-20">
             <div className="flex flex-col lg:flex-row items-center py-6 ">
               <div className="flex flex-col  xl:mb-0 mb-4 ">
-                <h1 className=" md:pb-0 pb-4 2xl:text-[56px] xl:text-[52px] xl:w-[650px] 2xl:w-[700px]  4xl:text-[60px] 4xl:w-[740px] lg:text-[42px] lg:w-[520px] leading-tight text-[26px] md:text-[32px] ">
+                <h1 className=" md:pb-0 pb-4  xl:w-[650px] 2xl:w-[700px] 4xl:w-[740px] lg:w-[520px] leading-tight 4xl:text-[60px] 2xl:text-[40px] xl:text-[40px] md:text-[30px] text-[26px]">
                   {t("simplifyTitle")}
                 </h1>
               </div>
               <div className="flex flex-col w-full ">
-                <p className="text-[#000000]  2xl:text-[18px] 2xl:w-[520px] 4xl:text-[20px] 4xl:w-[580px]  xl:text-[16px] xl:w-[460px] lg:text-[14px] lg:w-[400px] md:text-[18px] 4xl:pl-10 2xl:pl-8 xl:pl-8 lg:pl-6">
+                <p className="text-[#000000] 2xl:w-[520px] 4xl:w-[580px] xl:w-[460px] lg:w-[400px] 4xl:pl-10 2xl:pl-8 xl:pl-8 lg:pl-6 text-[16px] md:text-[18px] 2xl:text-[18px] xl:text-[18px] 4xl:text-[20px]">
                   {t("simplifyDesc")}
                 </p>
               </div>
@@ -113,10 +114,10 @@ export default function HealthCloud() {
         />
       </section>
       <section className="lg:mt-20 custom-container mt-10 pb-10">
-        <h2 className="text-[24px] md:text-[40px] lg:text-[40px] xl:text-[46px] 2xl:text-[54px] 4xl:text-[60px] leading-tight   font-normal">
+        <h2 className="4xl:text-[60px] 2xl:text-[40px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight font-normal">
           {t("streamlineTitle")}
         </h2>
-        <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 lg:mt-10 gap-5">
+        <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 lg:mt-10 gap-14">
           {salesForce.map((sales) => {
             return (
               <div key={sales.id}>
@@ -125,7 +126,7 @@ export default function HealthCloud() {
                     src={sales.imageUrl}
                     alt={sales.alt}
                     className="w-20 h-20"
-                    width={0} 
+                    width={0}
                     height={0}
                     sizes="100vw"
                     style={{
@@ -141,7 +142,7 @@ export default function HealthCloud() {
             );
           })}
         </div>
-        <h2 className="text-[24px] md:text-[40px] lg:text-[40px] xl:text-[46px] 2xl:text-[54px] 4xl:text-[60px] leading-tight  font-normal mt-20 lg:mt-40 2xl:w-[980px] 4xl:w-[1084px] xl:w-[860px] lg:w-[730px] ">
+        <h2 className="4xl:text-[60px] 2xl:text-[40px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight  font-normal mt-20 lg:mt-40 2xl:w-[980px] 4xl:w-[1084px] xl:w-[860px] lg:w-[730px] ">
           {t("pipelineTitle")}
         </h2>
         <div className="my-10">
@@ -150,19 +151,18 @@ export default function HealthCloud() {
               return (
                 <div
                   key={sales.id}
-                  className="border-2 p-10 hover:bg-[#D9F2FF] hover:border-transparent transition-all duration-300"
-                >
+                  className="border-2 p-10 hover:bg-[#D9F2FF] hover:border-transparent transition-all duration-300">
                   <h3 className="mt-5 text-[#1F3F69] text-[22px] md:text-[24px] lg:text-[20px] xl:text-[24px] 2xl:text-[28px] 4xl:text-[30px] md:w-[210px] lg:w-[230px] xl:w-[240px] 2xl:w-[260px] 4xl:w-[301px]">
                     {sales.title}
                   </h3>
-                  <p className="mt-5 text-[16px] md:text-[18px] lg:text-[14px] xl:text-[18px] 2xl:text-[20px] 4xl:text-[22px] 4xl:w-[304px] 2xl:w-[290px] xl:w-[260px] lg:w-[200px]">
+                  <p className="mt-5 text-[16px] md:text-[18px] 2xl:text-[18px] xl:text-[18px] 4xl:text-[20px] 4xl:w-[304px] 2xl:w-[290px] xl:w-[260px] lg:w-[200px]">
                     {sales.description}
                   </p>
                 </div>
               );
             })}
           </div>
-          <LearnMore />
+          <LearnMore btnName={t('learnMore')} />
         </div>
       </section>
       {/* Reduce Revenue Pitfalls, Supercharge Your Sales Funnel */}
@@ -173,19 +173,18 @@ export default function HealthCloud() {
               <h2 className="pb-4 text-black text-[24px] md:text-[24px] lg:text-[30px] xl:text-[34px] 2xl:text-[38px] 4xl:text-[40px] leading-tight font-normal 2xl:w-[1080px] 4xl:w-[1190px] xl:w-[1000px] lg:w-[990px]">
                 {t("funnelTitle")}
               </h2>
-              <p className="pb-8 text-[14px] md:text-[16px] lg:text-[16px] xl:text-[18px] 2xl:text-[18px] 4xl:text-[20px]">
+              <p className="pb-8 text-[16px] md:text-[18px] 2xl:text-[18px] xl:text-[18px] 4xl:text-[20px] leading-tight">
                 {t("funnelDesc")}
               </p>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {revenueStats.map((item, index) => (
                   <div
                     key={index}
-                    className={`flex flex-col ${item.widthClasses}`}
-                  >
-                    <h2 className="text-[#0092E0] font-bold text-4xl md:text-6xl mb-2 2xl:text-[56px] 4xl:text-[70px] xl:text-[50px] md:text-[40px] lg:text-[40px]">
+                    className={`flex flex-col ${item.widthClasses}`}>
+                    <h2 className="text-[#0092E0] font-bold mb-2 4xl:text-[60px] 2xl:text-[40px] xl:text-[40px] md:text-[30px] text-[26px]">
                       {item.value}
                     </h2>
-                    <p className="text-black 4xl:text-[24px] 2xl:text-[20px] lg:text-[16px] md:text-[20px] xl:text-[18px]">
+                    <p className="text-black text-[16px] md:text-[18px] 2xl:text-[18px] xl:text-[18px] 4xl:text-[20px]">
                       {item.description}
                     </p>
                   </div>
