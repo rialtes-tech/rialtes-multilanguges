78102ff938e791116c75b846836ba5ce66fd920b
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-12-24 18:28:06 +0530

    cluster blog translated

---
+++ src/app/[locale]/insights/blogs/salesforce-consulting-playbook-enterprise-growth/page.js
@@ -0,0 +1,184 @@
+"use client";
+import Image from "next/image";
+import FilteredBlogCarousel from '@/app/[locale]/components/FilteredLatestBlogCarousel'
+import useUrl from "@/app/[locale]/components/useUrl";
+import Seo from "@/app/[locale]/components/Seo";
+import Script from "next/script";
+import FAQAccordion from "@/app/[locale]/components/faqAccordion";
+import BlogSocialIcons from '@/app/[locale]/components/blogSocialIcons'
+import UnorderedList from "@/app/[locale]/components/unorderedList";
+import { useLocale, useTranslations } from "next-intl";
+import enContent from '../../../../../../messages/en/blogs.json';
+import esContent from '../../../../../../messages/es/blogs.json';
+import frContent from '../../../../../../messages/fr/blogs.json';
+import { changeLocalization } from "@/app/[locale]/components/changeLocalization";
+
+
+export default function Page() {
+    const fullUrl = "https://www.rialtes.com/insights/blogs/salesforce-consulting-playbook-enterprise-growth";
+    const currUrl = useUrl()
+    const t = useTranslations('salesforceConsulting')
+    const locale = useLocale();
+    const content = changeLocalization(locale, { en: enContent, es: esContent, fr: frContent });
+    const { mainData,schemaData, faqdata, whySalesforceData, salesforceData, howSalesforceData, commonData } = content.salesforceConsulting;
+
+    return (
+        <section className="min-h-screen">
+            <Seo
+                title={t('seoTitle')}
+                description={t('seoDescription')}
+                keywords="Partners, website, welcome"
+                canonical={`https://www.rialtes.com/${locale}/insights/blogs/salesforce-consulting-playbook-enterprise-growth/`}
+            />
+
+            <Script
+                id="schema-cluster"
+                type="application/ld+json"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+            />
+
+            <section className="relative 4xl:h-[638px] xl:h-[450px] 2xl:h-[500px] sm:h-[600px] md:h-[700px] overflow-hidden">
+                <div className="xl:block hidden">
+                    <Image
+                        src="/images/blog/business-growth-journey.webp"
+                        alt={t('bannerAlt')}
+                        fill
+                        priority
+                    />
+                </div>
+                <div className="xl:hidden block">
+                    <Image
+                        src="/images/blog/business-growth-journey-mobile.webp"
+                        alt={t('bannerAlt')}
+                        priority
+                        height={0}
+                        width={0}
+                        className="w-full h-auto object-cover"
+                    />
+                </div>
+            </section>
+
+            <section className="custom-container 4xl:mt-[80px] xl:mt-[60px] mt-[40px]">
+                <div className="grid lg:grid-cols-12">
+                    <div className="4xl:col-span-10 xl:col-span-10 lg:col-span-11">
+
+                        {/* date and icons */}
+                        <BlogSocialIcons fullUrl={fullUrl} topic={t('blogTopic')} date="23 December 2025" />
+
+                        {/* main blog */}
+                        <div className="xl:mt-[60px] mt-[42px]">
+                            <h1 className="4xl:text-[60px] 2xl:text-[48px] xl:text-[42px] md:text-[28px] text-[26px] leading-tight">{t('blogTitle')}</h1>
+
+                            <div className="xl:mt-[38px] mt-[33px]">
+                                {
+                                    mainData.map((data, ind) => {
+                                        return (
+                                            <p
+                                                key={ind}
+                                                className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data }}
+                                            />
+                                        )
+                                    })
+                                }
+
+                                {/* why salesforce section */}
+                                {
+                                    whySalesforceData.map((data, ind) => (
+                                        <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                            <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                            <p className="mt-[29px] xl:mt-[30px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                            <UnorderedList arrName={data.list} ulClassName="mt-5 space-y-2 pl-[26px] lg:pl-[46px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium list-disc" />
+                                            <p className="mt-[29px] xl:mt-[30px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc2 }}
+                                            />
+                                        </div>
+                                    ))
+                                }
+
+                                {/* salesforce section */}
+                                {
+                                    salesforceData.map((data, ind) => (
+                                        <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                            <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                            <p
+                                                className="mt-5 md:mt-10 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                            {
+                                                data?.list?.map((elem, id) => (
+                                                    <div key={id}>
+                                                        <div className="flex items-center w-full mt-14 gap-5 md:gap-10">
+                                                            <div className="flex items-center justify-center w-12 h-10 md:w-16 md:h-16 bg-[#1486E6] text-white font-bold 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px]">
+                                                                {id + 1}
+                                                            </div>
+
+                                                            <div>
+                                                                <h3 className={`font-bold text-[18px] md:text-[19px] xl:text-[19px] 4xl:text-[22px]`} dangerouslySetInnerHTML={{ __html: elem.title }} />
+                                                            </div>
+
+                                                            <div className="flex-1 border-t border-[#707070] md:block hidden"></div>
+                                                        </div>
+                                                        <UnorderedList arrName={elem.desc} ulClassName={`mt-[29px] xl:mt-[30px] space-y-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium`} />
+
+                                                        <ul className={`mt-5 space-y-1 pl-[26px] lg:pl-[46px] list-disc`}>
+                                                            {elem.listData.map((data, idx) => (
+                                                                <li key={idx}><h4 className="4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium">{data}</h4></li>
+                                                            ))}
+                                                        </ul>
+
+                                                        <p
+                                                            className="mt-5 md:mt-10 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                            dangerouslySetInnerHTML={{ __html: elem.desc2 }}
+                                                        />
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    ))
+                                }
+
+                                {/* how salesforce section */}
+                                <div className="md:mt-[50px] mt-[40px]">
+                                    <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{t('howSalesforceTitle')}</h2>
+                                    <p className="mt-[29px] xl:mt-[30px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]">{howSalesforceData}</p>
+                                </div>
+
+                                {/* common section */}
+                                {
+                                    commonData.map((data, ind) => (
+                                        <div className="md:mt-[50px] mt-[40px]" key={ind}>
+                                            <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{data.title}</h2>
+                                            <p className="mt-[29px] xl:mt-[30px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc }}
+                                            />
+                                            <UnorderedList arrName={data.list} ulClassName="mt-5 space-y-2 pl-[26px] lg:pl-[46px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium list-disc" />
+                                            <p className="mt-[29px] xl:mt-[30px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px]"
+                                                dangerouslySetInnerHTML={{ __html: data.desc2 }}
+                                            />
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* faq section */}
+            <section className="xl:mt-[80px] mt-[40px] custom-container">
+                <h2 className="font-semibold text-[#0092E0] 4xl:text-[32px] 2xl:text-[26px] xl:text-[26px] md:text-[22px] text-[22px] leading-tight">{t('faqTitle')}</h2>
+                <div className="mt-[29px] xl:mt-[30px]">
+                    <FAQAccordion faqData={faqdata} />
+                </div>
+            </section>
+
+            {/* blog carousel */}
+            <section className="custom-container lg:pr-0 xl:my-[80px] md:my-[60px] my-[40px]">
+                <FilteredBlogCarousel url={currUrl} />
+            </section>
+        </section>
+    )
+}
