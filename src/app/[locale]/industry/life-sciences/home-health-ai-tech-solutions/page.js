9851ba692b1812db5422d2f61a3a5114d09c76e2
Nikita Kothiwala <nikita.k@rialtes.com>
2026-01-06 20:14:32 +0530

    updated text font size on 4 pages

--- src/app/[locale]/industry/life-sciences/home-health-ai-tech-solutions/page.js
+++ src/app/[locale]/industry/life-sciences/home-health-ai-tech-solutions/page.js
@@ -57,9 +57,9 @@ export default function Page() {
                 <div className="relative h-full custom-container flex items-center">
                     <div className="grid grid-cols-1 lg:grid-cols-12 w-full pt-[80px] md:pt-0">
                         <div className="col-span-12">
-                            <h1 className="text-[#FFFFFF] text-[18px] md:text-[24px] font-bold lg:w-full">
+                            <h1 className="text-[#FFFFFF] text-[18px] xl:text-[20px] 4xl:text-[24px] font-bold lg:w-full">
                                 {t('headerTitle')}
-                                <div className="text-[#FFFFFF] text-[26px] leading-tight xl:text-[40px] 4xl:text-[60px] mt-[11.5px] md:mt-[28.5px]">
+                                <div className="text-[#FFFFFF] text-[26px] md:text-[30px] leading-tight xl:text-[40px] 4xl:text-[60px] mt-[11.5px] md:mt-[28.5px]">
                                     {t('headerSubTitle')}    <div className="min-[330px]:block hidden" />{t('headerSubTitle2')}  <div className="min-[330px]:block hidden" />{t('headerSubTitle3')}
                                 </div>
                             </h1>
@@ -71,14 +71,14 @@ export default function Page() {
             </section>
             {/* page description section */}
             <div className="custom-container xl:mt-[124px] mt-[53px]">
-                <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-[40px] gap-y-[28px]">
+                <div className="grid lg:grid-cols-12 grid-cols-1 xl:gap-[30px] 4xl:gap-[40px] gap-y-[28px]">
                     <div className="lg:col-span-6 xl:col-span-7 col-span-12">
-                        <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight md:w-[80%] xl:w-full">
+                        <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight md:w-[80%] xl:w-[90%] 4xl:w-full">
                             {t('pageDescTitle')}
                         </h2>
                     </div>
                     <div className="lg:col-span-6 xl:col-span-5 col-span-12">
-                        <p className="text-[16px] 4xl:text-[22px] xl:text-[18px] font-normal mt-[6px] w-[90%] md:w-[80%] lg:w-full">
+                        <p className="text-[16px] 4xl:text-[22px] md:text-[18px] xl:text-[18px] font-normal mt-[6px] w-[90%] md:w-[80%] lg:w-full">
                             {t('pageDesc')}
                         </p>
                     </div>
@@ -86,10 +86,10 @@ export default function Page() {
             </div>
             {/* addressing section */}
             <section className="custom-container xl:mt-[166px] mt-[49px]">
-                <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight lg:w-[60%] 4xl:w-[70%]">
+                <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight lg:w-[60%] 4xl:w-[70%]">
                     {t('addressingTitle')}
                 </h2>
-                <p className="text-[16px] 4xl:text-[22px] xl:text-[18px] font-normal xl:mt-[39px] mt-[28px] w-[90%] lg:w-[65%] 4xl:w-[70%]">
+                <p className="text-[16px] 4xl:text-[22px] md:text-[18px] xl:text-[18px] font-normal xl:mt-[39px] mt-[28px] w-[90%] lg:w-[65%] 4xl:w-[70%]">
                     {t('addressingDesc')}
                 </p>
                 {
@@ -104,8 +104,8 @@ export default function Page() {
                                     </div>
                                 </div>
                                 <div className={`lg:col-span-8 col-span-12 xl:pt-[51px] xl:pb-[93px] xl:px-[68px] pt-[29px] pb-[64px] px-[30px] relative  ${!isEven && "lg:order-1 order-2"}`} key="text">
-                                    <h2 className="4xl:text-[30px] xl:text-[26px] text-[22px] leading-tight text-[#0A6BB8] font-semibold w-[90%] md:w-full">{data.title}</h2>
-                                    <p className="text-[16px] 4xl:text-[22px] xl:text-[18px] font-normal xl:mt-[39px] mt-[19px] w-[90%] lg:w-[94%]">{data.desc}</p>
+                                    <h2 className="4xl:text-[30px] xl:text-[24px] text-[22px] leading-tight text-[#0A6BB8] font-semibold w-[90%] md:w-full">{data.title}</h2>
+                                    <p className="text-[16px] 4xl:text-[22px] md:text-[18px] xl:text-[18px] font-normal xl:mt-[39px] mt-[19px] w-[90%] lg:w-[94%]">{data.desc}</p>
                                     <div className="xl:mt-[40px] mt-[20px] absolute"><LearnMore btnName={t('learnMoreBtn')} locale={locale}/></div>
                                 </div>
                             </React.Fragment>
@@ -125,8 +125,8 @@ export default function Page() {
                 <div className="grid lg:grid-cols-12 grid-cols-1">
                     <div className="lg:col-span-5 col-span-12 lg:my-[40px] max-md:mx-[36px] lg:mr-[-90px] max-md:mt-[-120px] relative z-20 lg:order-1 order-2">
                         <div className="bg-[#006FBE] text-[#ffffff] xl:pt-[69px] xl:pb-[96px] lg:pl-[63px] xl:pr-[20px] pt-[39px] pb-[70px] px-[26px]">
-                            <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight font-light w-[95%] lg:w-[60%] xl:w-full">{t('globalTitle')}</h2>
-                            <p className="text-[16px] 4xl:text-[22px] xl:text-[18px] font-normal xl:mt-[65px] mt-[25px] w-[94%] lg:w-[90%]">{t('globalSubTitle')}</p>
+                            <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight font-light w-[95%] lg:w-[60%] xl:w-full">{t('globalTitle')}</h2>
+                            <p className="text-[16px] 4xl:text-[22px] md:text-[18px] xl:text-[18px] font-normal xl:mt-[65px] mt-[25px] w-[94%] lg:w-[90%]">{t('globalSubTitle')}</p>
                             <div className="absolute xl:mt-[40px] mt-[20px]">
                                 <LearnMore btnName={t('learnMoreBtn')} locale={locale} />
                             </div>
@@ -142,12 +142,12 @@ export default function Page() {
             <section className="custom-container xl:mt-[119px] mt-[85px]">
                 <div className="grid xl:grid-cols-12 grid-cols-1 gap-y-[29px] xl:gap-[60px]">
                     <div className="xl:col-span-7 col-span-12">
-                        <h2 className="4xl:text-[55px] xl:text-[40px] text-[26px] leading-tight">
+                        <h2 className="4xl:text-[55px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight">
                             {t('howAiTitle')}
                         </h2>
                     </div>
                     <div className="xl:col-span-5 col-span-12">
-                        <p className="text-[16px] 4xl:text-[20px] xl:text-[18px] font-normal md:w-[80%] xl:w-full">{t('howAiDesc')}</p>
+                        <p className="text-[16px] 4xl:text-[20px] md:text-[18px] xl:text-[16px] font-normal md:w-[80%] xl:w-full">{t('howAiDesc')}</p>
                     </div>
                 </div>
                 <div className="xl:mt-[65px] mt-[39px]">
@@ -156,10 +156,10 @@ export default function Page() {
                             aiRovolutionizingData.map((data, ind) => {
                                 return (
                                     <div key={ind} className="border border-[#707070] xl:pt-[52px] xl:pb-[56px] xl:px-[40px] pt-[32px] pb-[32px] px-[26px]">
-                                        <h3 className="4xl:text-[30px] xl:text-[26px] text-[20px] leading-tight text-[#006FBE] font-semibold">
+                                        <h3 className="4xl:text-[30px] xl:text-[24px] md:text-[22px] text-[20px] leading-tight text-[#006FBE] font-semibold">
                                             {data.title}
                                         </h3>
-                                        <p className="text-[16px] 4xl:text-[22px] xl:text-[18px] font-normal xl:mt-[31px] mt-[22px] w-[90%] xl:w-full 4xl:w-[86%]">
+                                        <p className="text-[16px] 4xl:text-[22px] md:text-[18px] xl:text-[18px] font-normal xl:mt-[31px] mt-[22px] w-[90%] xl:w-full 4xl:w-[86%]">
                                             {data.desc}
                                         </p>
                                     </div>
@@ -176,17 +176,17 @@ export default function Page() {
                 alt={t('aiPoweredAlt')}>
 
                 <div className="custom-container text-[#ffffff]">
-                    <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight">
+                    <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight">
                         {t('ourCapabTitle')}
                     </h2>
                     <div className="grid lg:grid-cols-12 grid-cols-1 xl:mt-[36px] mt-[17px] lg:gap-[60px] gap-y-[17px]">
                         <div className="lg:col-span-5 col-span-12">
-                            <h3 className="4xl:text-[42px] xl:text-[32px] text-[22px] leading-tight">
+                            <h3 className="4xl:text-[42px] xl:text-[36px] md:text-[26px] text-[22px] leading-tight">
                                 {t('ourCapabSubTitle')}
                             </h3>
                         </div>
                         <div className="lg:col-span-7 col-span-12">
-                            <p className="text-[16px] 4xl:text-[20px] xl:text-[18px] font-normal w-[90%] md:w-[70%] lg:w-full 4xl:w-[90%]">
+                            <p className="text-[16px] 4xl:text-[20px] xl:text-[16px] md:text-[18px] font-normal w-[90%] md:w-[70%] lg:w-full 4xl:w-[90%]">
                                 {t('ourCapabDesc')}
                             </p>
                         </div>
@@ -197,13 +197,13 @@ export default function Page() {
                             capabilitiesData.map((data, ind) => {
                                 return (
                                     <div key={ind}>
-                                        <h4 className="4xl:text-[36px] xl:text-[28px] text-[20px] leading-tight w-[90%] lg:w-full">
+                                        <h4 className="4xl:text-[36px] xl:text-[30px] md:text-[22px] text-[20px] leading-tight w-[90%] lg:w-full">
                                             {data.title}
                                         </h4>
-                                        <p className="text-[16px] 4xl:text-[20px] xl:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[35px] mt-[16px] 4xl:w-[98%]">
+                                        <p className="text-[16px] 4xl:text-[20px] md:text-[18px] xl:text-[16px] font-normal w-[90%] lg:w-full xl:mt-[35px] mt-[16px] 4xl:w-[98%]">
                                             {data.desc && data.desc}
                                         </p>
-                                        <UnorderedList arrName={data.dataList} ulClassName="xl:space-y-[24px] space-y-[19px] list-disc xl:mt-[35px] mt-[17px] pl-[20px] w-[90%] md:w-full 4xl:w-[90%]" liClassName="text-[16px] xl:text-[18px] 4xl:text-[20px] leading-tight font-normal" />
+                                        <UnorderedList arrName={data.dataList} ulClassName="xl:space-y-[24px] space-y-[19px] list-disc xl:mt-[35px] mt-[17px] pl-[20px] w-[90%] md:w-full 4xl:w-[90%]" liClassName="text-[16px] xl:text-[16px] md:text-[18px] 4xl:text-[20px] leading-tight font-normal" />
                                     </div>
                                 )
                             })
@@ -222,13 +222,13 @@ export default function Page() {
                         <Image src="/images/home-health/agent-chat-desk.webp" alt={t('agentchatAlt')} className="hidden md:block w-full h-full object-cover md:h-[600px] lg:h-full" width={0} height={0} priority />
                     </div>
                     <div>
-                        <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight lg:mt-[46px]">
+                        <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight lg:mt-[46px]">
                             {t('agentChatTitle')}
                         </h2>
-                        <h3 className="4xl:text-[42px] xl:text-[32px] text-[22px] leading-tight xl:mt-[35px] mt-[17px] font-semibold w-[85%] md:w-full">
+                        <h3 className="4xl:text-[42px] xl:text-[36px] md:text-[26px] text-[22px] leading-tight xl:mt-[35px] mt-[17px] font-semibold w-[85%] md:w-full">
                             {t('agentChatSubTitle')}
                         </h3>
-                        <p className="text-[16px] 4xl:text-[20px] xl:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[35px] mt-[24px]">
+                        <p className="text-[16px] 4xl:text-[20px] xl:text-[16px] md:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[35px] mt-[24px]">
                             {t('agentChatDesc')}
                         </p>
                     </div>
@@ -245,10 +245,10 @@ export default function Page() {
                                 <div
                                     key={ind}
                                     className={`border-l-2 border-[#ffffff] xl:pl-[32px] pl-[18px] ${getXlPaddingRight(ind)}`}>
-                                    <h4 className="4xl:text-[32px] xl:text-[28px] text-[20px] leading-tight w-[90%] lg:w-full">
+                                    <h4 className="4xl:text-[32px] xl:text-[26px] md:text-[22px] text-[20px] leading-tight w-[90%] lg:w-full">
                                         {data.title}
                                     </h4>
-                                    <p className="text-[16px] 4xl:text-[20px] xl:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[21px] mt-[19px] 4xl:w-[98%]">
+                                    <p className="text-[16px] 4xl:text-[20px] md:text-[18px] xl:text-[16px] font-normal w-[90%] lg:w-full xl:mt-[21px] mt-[19px] 4xl:w-[98%]">
                                         {data.desc && data.desc}
                                     </p>
                                 </div>
@@ -259,12 +259,12 @@ export default function Page() {
                 <div className="relative">
                     <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-[60px] gap-y-[19px] mt-[40px] xl:mt-[80px] items-stretch">
                         <div className="lg:col-span-3 col-span-12">
-                            <h4 className="4xl:text-[36px] xl:text-[28px] text-[20px] leading-tight w-[90%] lg:w-full">
+                            <h4 className="4xl:text-[36px] xl:text-[30px] md:text-[22px] text-[20px] leading-tight w-[90%] lg:w-full">
                                 {t('agentChatSubTitle2')}
                             </h4>
                         </div>
                         <div className="lg:col-span-6 xl:col-span-7 col-span-12">
-                            <UnorderedList arrName={homeHealth} ulClassName="xl:space-y-[24px] space-y-[19px] list-disc pl-[20px] w-[90%] md:w-full 4xl:w-[90%] lg:pb-[120px]" liClassName="text-[16px] xl:text-[18px] 4xl:text-[20px] leading-tight font-normal" />
+                            <UnorderedList arrName={homeHealth} ulClassName="xl:space-y-[24px] space-y-[19px] list-disc pl-[20px] w-[90%] md:w-full 4xl:w-[90%] lg:pb-[120px]" liClassName="text-[16px] xl:text-[16px] md:text-[18px] 4xl:text-[20px] leading-tight font-normal" />
                         </div>
                         <div className="xl:col-span-2 lg:col-span-3 col-span-12 h-full">
 
@@ -279,7 +279,7 @@ export default function Page() {
 
             {/* related section */}
             <section className="custom-container xl:mt-[125px] mt-[64px]">
-                <h2 className="4xl:text-[60px] xl:text-[40px] text-[26px] leading-tight">
+                <h2 className="4xl:text-[60px] xl:text-[40px] md:text-[30px] text-[26px] leading-tight">
                     {t('relatedTitle')}
                 </h2>
                 <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-x-[60px] xl:gap-x-0 gap-y-[40px] xl:mt-[83px] mt-[40px] sm:w-[80%] xl:w-full">
@@ -294,10 +294,10 @@ export default function Page() {
                             };
                             return (
                                 <div className={`${paddingRight(ind)} flex flex-col pb-[30px] xl:pb-0`} key={ind}>
-                                    <h3 className="4xl:text-[30px] xl:text-[25px] text-[20px] leading-tight font-semibold text-[#006FBE]">
+                                    <h3 className="4xl:text-[30px] xl:text-[24px]  text-[20px] leading-tight font-semibold text-[#006FBE]">
                                         {data.title}
                                     </h3>
-                                    <p className="text-[16px] 4xl:text-[24px] xl:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[31px] mt-[10px] xl:mb-[31px] mb-[18px]">
+                                    <p className="text-[16px] md:text-[18px] 4xl:text-[24px] xl:text-[18px] font-normal w-[90%] lg:w-full xl:mt-[31px] mt-[10px] xl:mb-[31px] mb-[18px]">
                                         {data.desc}
                                     </p>
                                     <div className="mt-auto">
@@ -311,7 +311,7 @@ export default function Page() {
             </section>
             {/* Contact Form */}
             <section className="custom-container xl:mt-[130px] xl:mb-[130px] mt-[65px] mb-[48px]">
-                <ContactForm title={t('contactTitle')} subtitle={t('contactSubTitle')} className={"font-normal text-[26px] leading-tight xl:text-[40px] 4xl:text-[60px] xl:w-[60%] 4xl:w-[80%]"} />
+                <ContactForm title={t('contactTitle')} subtitle={t('contactSubTitle')} className={"font-normal text-[26px] leading-tight xl:text-[40px] md:text-[30px] 4xl:text-[60px] xl:w-[60%] 4xl:w-[80%]"} />
             </section >
         </section>
     )
