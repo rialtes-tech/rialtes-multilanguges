e0109ef7b2c7bbcf0438305cb71b3b80ac5ce378
alkadips <alka.n@rialtes.com>
2026-01-07 18:27:10 +0530

    added some qa changes

--- src/app/insights/blogs/grow-with-sap-ai-ready-cloud-erp/page.js
+++ src/app/insights/blogs/grow-with-sap-ai-ready-cloud-erp/page.js
@@ -389,7 +389,7 @@ export default function Page() {
                                         </div>
                                     ))
                                 }
-                                <div className="bg-[#006FBE] text-white font-semibold p-5 mt-16 4xl:text-[26px] 2xl:text-[22px] xl:text-[22px] md:text-[20px] text-[20px]">GROW with SAP = Modern ERP + Standardization + Future AI Enablement</div>
+                                <div className="bg-[#006FBE] text-white 2xl:w-[90%] 4xl:w-full font-semibold p-5 mt-16 4xl:text-[26px] 2xl:text-[22px] xl:text-[22px] md:text-[20px] text-[20px]">GROW with SAP = Modern ERP + Standardization + Future AI Enablement</div>
                                 <div className="xl:block hidden md:block">
                                     <img
                                         src="/images/blog/voygernext/infographic11.png"
@@ -443,7 +443,7 @@ export default function Page() {
                                                     data.list.map((elem, index) => {
                                                         return (
                                                             <div className="4xl:p-[30px] p-[32px] border border-[#707070]" key={`${elem.title}-${index}`}>
-                                                                <h3 className="text-[#006FBE] 4xl:text-[22px] 2xl:text-[21px] xl:text-[20px] md:text-[20px] text-[20px] font-bold pb-4 border-b border-[#707070] leading-tight">{elem.title}</h3>
+                                                                <h3 className="text-[#006FBE] 4xl:text-[22px] 2xl:text-[19px] xl:text-[20px] md:text-[20px] text-[20px] font-bold pb-4 border-b border-[#707070] leading-tight">{elem.title}</h3>
                                                                 <p className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] leading-tight" dangerouslySetInnerHTML={{ __html: elem.desc }} />
                                                             </div>
                                                         )
@@ -471,7 +471,7 @@ export default function Page() {
                                         </div>
                                     ))
                                 }
-                                <table className="4xl:w-[80%] 2xl:w-[85%] border-collapse mt-10">
+                                <table className="4xl:w-[80%] 2xl:w-[85%] xl:w-[90%] border-collapse mt-10">
                                     {/* Header */}
                                     <thead className="bg-gray-100 border-b border-gray-500">
                                         <tr>
@@ -519,7 +519,7 @@ export default function Page() {
                                                     data.list.map((elem, index) => {
                                                         return (
                                                             <div className="4xl:p-[30px] p-[32px] border border-[#707070]" key={`${elem.title}-${index}`}>
-                                                                <h3 className="text-[#006FBE] 4xl:text-[22px] 2xl:text-[21px] xl:text-[20px] md:text-[20px] text-[20px] font-bold pb-4 leading-tight">{elem.title}</h3>
+                                                                <h3 className="text-[#006FBE] 4xl:text-[22px] 2xl:text-[19px] xl:text-[20px] md:text-[20px] text-[20px] font-bold pb-4 leading-tight">{elem.title}</h3>
                                                                 <p className="mt-5 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] leading-tight" dangerouslySetInnerHTML={{ __html: elem.desc }} />
                                                             </div>
                                                         )
@@ -540,7 +540,7 @@ export default function Page() {
                                             {
                                                 data.list.map((elem, index) => (
                                                     <div key={`${elem.title}-${index}`} className="mt-8 pl-6">
-                                                        <h3 className="4xl:text-[20px] 2xl:text-[19px] text-[20px] font-bold mt-8">{index + 1}. {elem.title}</h3>
+                                                        <h3 className="4xl:text-[20px] 2xl:text-[17px] text-[20px] font-bold mt-8">{index + 1}. {elem.title}</h3>
                                                         <UnorderedList arrName={elem.desc} ulClassName={`md:pl-[24px] 4xl:text-[20px] 2xl:text-[17px] xl:text-[17px] md:text-[16px] text-[16px] font-medium`} />
                                                     </div>
                                                 ))
